7a6894c4a5e74c5b345a23e4d27a34d6cc3f618e
Jack <jack@fin1te.net>
2020-07-23 15:34:04 +0100

    Update __init__.py

--- py/__init__.py
+++ py/__init__.py
@@ -8,3 +8,17 @@ bp = Blueprint('test', __name__, url_prefix='/abc')
 def test():
     user_data = request.args.get('user')
     requests.get(user_data)
+
+@bp.route('/test2')
+def test2():
+    user_data = request.args.get('user')
+    print(user_data)
+
+@bp.route('/test3')
+def test3():
+    requests.get("/")
+
+@bp.route('/test4')
+def test4():
+    user_data = request.args['user']
+    requests.get(user_data)
